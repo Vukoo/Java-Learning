e205b14c48928670ad2ae5746de595758880d814
Vukoo <piotrgadek4@gmail.com>
2019-02-18 09:00:56 +0100

    Triangle read input and calculate area

---
+++ src/gadek/com/Triangle.java
@@ -0,0 +1,59 @@
+package gadek.com;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.naming.InvalidNameException;
+
+public class Triangle {
+
+		private double[] number;
+		
+		public Triangle( double a, double b, double c) {
+			this.number = new double[] {a,b,c};
+		}
+		
+		public Triangle( ) {
+		}
+		
+		public double area() {
+			Arrays.sort(number);
+			
+			if(isTrangle())
+			return (number[0]*number[1])/2;
+			else return -1;
+		}
+		
+		public boolean isTrangle() {
+			return number[0] + number[1] > number[2] & 
+					number[2] + number[1] > number[0] & 
+					number[0] + number[2] > number[1];
+		}
+		
+		public Double Equilateral() {
+			return (Math.pow(number[0],2)* Math.sqrt(3))/4;
+		}
+		
+		
+		public void readPositiveInt() throws InvalidNameException {
+			
+			String[] textTab= new String[3];
+			Scanner sc = new Scanner(System.in);
+			try {
+			 textTab = sc.nextLine().split(" ");
+			 sc.close();
+			 number = new double[] {Double.parseDouble(textTab[0]),Double.parseDouble(textTab[1]),Double.parseDouble(textTab[2])};
+			}
+			catch (NumberFormatException e){ 
+				System.out.println("Pole z³y format! Wpisz ponownie");
+				readPositiveInt();
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+	           // throw new InvalidNameException("Missing number ");
+				System.out.println("Wpisz trzy cyfry!");
+	            readPositiveInt();
+			}
+			
+		}
+
+}
