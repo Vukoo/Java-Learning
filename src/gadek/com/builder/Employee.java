40924949e054ba601aea7a3fe698054e2790b5fd
Vukoo <piotrgadek4@gmail.com>
2019-02-18 08:55:37 +0100

    Simple builder pattern

---
+++ src/gadek/com/builder/Employee.java
@@ -0,0 +1,101 @@
+package gadek.com.builder;
+
+import java.util.Date;
+
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private Gender gender;
+	private String country;
+	private Integer age;
+	private String zipCode;
+	
+	private Employee(String firstName, String lastName, Gender gender, String country, Integer age, String zipCode) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.country = country;
+		this.age = age;
+		this.zipCode = zipCode;
+	}
+	 
+
+		public static class Builder {
+	    	private String firstName;
+	    	private String lastName;
+	    	private Gender gender;
+	    	private String country;
+	    	private Integer age;
+	    	private String zipCode;
+	    	
+	    	public Builder() {
+	    	}
+
+			public Builder setFirstName(String firstName) {
+				this.firstName = firstName;
+				return this;
+			}
+
+			public Builder  setLastName(String lastName) {
+				this.lastName = lastName;
+				return this;
+			}
+
+			public Builder  setGender(Gender gender) {
+				this.gender = gender;
+				return this;
+			}
+
+			public Builder  setCountry(String country) {
+				this.country = country;
+				return this;
+
+			}
+
+			public Builder  setBirth(Integer age) {
+				this.age = age;
+				return this;
+			}
+
+			public Builder  setZipCode(String zipCode) {
+				this.zipCode = zipCode;
+				return this;
+			}
+	        
+			public Employee build() {
+				return new Employee(firstName, lastName, gender, country, age, zipCode);
+			}
+	    }
+	    
+	 public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", gender=" + gender + ", country="
+				+ country + ", age=" + age + ", zipCode=" + zipCode + "]";
+	}  
+}
