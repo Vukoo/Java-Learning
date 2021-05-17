d4eb71194afde88a643d76421e47a5760ef74faa
Piotr Gądek <piotrgadek4@gmail.com>
2021-05-17 23:11:13 +0200

    simple kata before bed

--- src/main/java/gadek/com/Kata.java
+++ src/main/java/gadek/com/Kata.java
@@ -2,13 +2,20 @@ package gadek.com;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.IntStream.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Kata {
-
+    public static void main(String[] args) {
+        solve2(new int[]{3,4,4,3,6,3});
+    }
 
     //	You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
     public static boolean smallEnough(int[] a, int limit)
@@ -40,6 +47,35 @@ public class Kata {
         }
         return result;
 //        retur input.replaceAll("([A-Z])", " $1");
+    }
+    public static int [] solve(int [] arr){
+        return Arrays.stream(arr).distinct().toArray();
+    }
+    public static int [] solve2(int [] arr){
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < arr.length ; i++) {
+            int temp = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if(temp == arr[j]){
+                    break;
+                } else if(arr.length == j+1){
+                    arrayList.add(temp);
+                }
+            }
+        }
+        arrayList.add(arr[arr.length-1]);
+
+        int[] array = arrayList.stream().mapToInt(i->i).toArray();
+        return array;
+    }
+    public static int[] solve3(int[] arr) {
+        LinkedList<Integer> unique = new LinkedList<Integer>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!unique.contains(arr[i])) {
+                unique.addFirst(arr[i]);
+            }
+        }
+        return unique.stream().mapToInt(i -> i).toArray();
     }
 
 
